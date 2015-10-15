420b854760c5f8ce9498563e7896843a071b743d
Hermann Hueck <hermann.hueck@secure.mailbox.org>
2015-10-15 15:11:01 +0200

    Added Redirect from '/' to '/videoApp'

--- VideoServer-Play-Scala-Slick-NoAuth/app/videosvc/controllers/WebApplication.scala
+++ VideoServer-Play-Scala-Slick-NoAuth/app/videosvc/controllers/WebApplication.scala
@@ -4,8 +4,20 @@ import play.api.mvc._
 
 class WebApplication extends Controller {
 
+/*
   def index = Action {
     Ok(videosvc.views.html.index("Your new application is ready."))
   }
+*/
+
+  def index = Action {
+    Redirect("/videoApp")
+  }
+
+  def app = TODO
+
+//  def app = Action {
+//    Ok(videosvc.views.html.index("Your new application is ready."))
+//  }
 
 }
--- VideoServer-Play-Scala-Slick-NoAuth/test/videosvc/test/WebApplicationSpec.scala
+++ VideoServer-Play-Scala-Slick-NoAuth/test/videosvc/test/WebApplicationSpec.scala
@@ -3,6 +3,7 @@ package videosvc.test
 import org.junit.runner._
 import org.specs2.mutable._
 import org.specs2.runner._
+import play.api.Logger
 import play.api.test.Helpers._
 import play.api.test._
 
@@ -12,21 +13,50 @@ import play.api.test._
  * For more information, consult the wiki.
  */
 @RunWith(classOf[JUnitRunner])
-class WebApplicationSpec extends Specification {
+class WebApplicationSpec extends PlaySpecification {
+
+  val l: Logger = Logger(this.getClass)
 
   "WebApplication" should {
 
     "send 404 on a bad request" in new WithApplication {
-      route(FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
+
+      l.debug("(in Test 1) send 404 on a bad request")
+
+      val ofResult = route(FakeRequest(GET, "/boum"))
+
+      l.debug("headers = [[" + await(ofResult.get).toString() + "]]")
+      // l.debug("contentAsString = [[" + contentAsString(fResult) + "]]")
+
+
+      ofResult must beSome.which (status(_) == NOT_FOUND)
     }
 
-    "render the index page" in new WithApplication {
+    "redirect to /videoApp" in new WithApplication {
 
-      val home = route(FakeRequest(GET, "/")).get
+      l.debug("(in Test 2) redirect to /videoApp")
 
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+      val fResult = route(FakeRequest(GET, "/")).get
+
+      l.debug("headers = [[" + await(fResult).toString() + "]]")
+      // l.debug("contentAsString = [[" + contentAsString(fResult) + "]]")
+
+      status(fResult) must equalTo(SEE_OTHER)
+    }
+
+    "render the TODO page" in new WithApplication {
+
+      l.debug("(in Test 3) render the TODO page")
+
+      val fResult = route(FakeRequest(GET, "/videoApp")).get
+
+      l.debug("headers = [[" + await(fResult).toString() + "]]")
+      // l.debug("contentAsString = [[" + contentAsString(fResult) + "]]")
+
+      status(fResult) must equalTo(NOT_IMPLEMENTED)
+      contentType(fResult) must beSome.which(_ == "text/html")
+      contentAsString(fResult) must contain ("TODO")
+      contentAsString(fResult) must contain ("Action not implemented yet")
     }
   }
 }
